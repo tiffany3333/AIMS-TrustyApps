88110f6785e8b243ae0f0665ccf83e3a3e7ecd24
LeaderTNT <trong.tin.le.cs@gmail.com>
2016-06-27 12:12:42 -0400

    Create new project for data layer Create IdentityModels class Modify ConnectionString

---
+++ AIMS.Data/IdentityModels.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AIMS.Data
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class AIMSDbContext : IdentityDbContext
+    {
+        public AIMSDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static AIMSDbContext Create()
+        {
+            return new AIMSDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder
+                .Configurations
+                    .Add(new IdentityUserLoginConfiguration())
+                    .Add(new IdentityUserRoleConfiguration());
+        }
+
+        //public DbSet<ShoppingList> ShoppingLists { get; set; }
+
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.RoleId);
+        }
+    }
+}
