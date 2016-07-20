692066545ab521e3822c453ce33f6be891252f12
LeaderTNT <trong.tin.le.cs@gmail.com>
2016-07-19 23:15:30 -0400

    Modify EntityRole to prevent infinite loop Seed EntityRole with default record

---
+++ AIMS.Services/UserServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS.Services
+{
+    public class UserServices
+    {
+
+        private readonly Guid _userId;
+
+        public UserServices()
+        {
+        }
+
+    }
+}
