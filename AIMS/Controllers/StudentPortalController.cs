7d2a8848422424186262607526a39735b5be9722
Blake Steel <BlakeMSteel@gmail.com>
2016-10-27 21:43:37 -0400

    Links Work
    Links to new pages-
    AdminPortal
    StudentPortal
    -Are operational from the home page. 
    Have yet to do css. Tried. Failed. Committing working project before sleeping.

---
+++ AIMS/Controllers/StudentPortalController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AIMS.Controllers
+{
+    public class StudentPortalController : Controller
+    {
+        // GET: StudentPortal
+        public ActionResult StudentPortal()
+        {
+            return View();
+        }
+    }
+}
--- AIMS/Controllers/SurveyController.cs
+++ AIMS/Controllers/SurveyController.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
-using AIMS.Models;
+﻿using AIMS.Models;
 using AIMS.Services;
+using System;
 using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace AIMS.Controllers
 {
