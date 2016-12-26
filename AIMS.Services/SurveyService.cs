580b46889c08ba0e8db3dc6fcec155d162e8721c
Tiffany Trusty <tiffany@trustywebdev.com>
2016-12-26 12:38:38 -0500

    updated response images to be stored on azure blob

--- AIMS.Services/SurveyService.cs
+++ AIMS.Services/SurveyService.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using AIMS.Models;
 using System.Web;
 using System.IO;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
 
 namespace AIMS.Services
 {
@@ -197,7 +201,7 @@ namespace AIMS.Services
 
         public string uploadImage(HttpPostedFileBase uploadFile)
         {
-            string filePath = null;
+            string blobUri = null;
 
             if (uploadFile != null && uploadFile.ContentLength > 0)
             {
@@ -216,12 +220,103 @@ namespace AIMS.Services
                 else
                 {
                     var fileName = Path.GetFileName(uploadFile.FileName);
-                    filePath = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/Content/Upload"), fileName);
-                    uploadFile.SaveAs(filePath);                    
+
+                    //// START Temp local storage solution 
+                    //    filePath = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/Content/Upload"), fileName);
+                    //    //ensure filePath exists
+                    //    if (!System.IO.Directory.Exists(filePath))
+                    //    {
+                    //        System.IO.Directory.CreateDirectory(filePath);
+                    //    }
+                    //    uploadFile.SaveAs(filePath);
+                    //// END Temp local storage solution
+                    BlobHandler bh = new BlobHandler("images");
+                    blobUri = bh.Upload(uploadFile);
+                    //var blobUris = bh.GetBlobs();
+                    
                 }
             }
 
-            return filePath;
+            return blobUri;
+        }
+    }
+
+    public class BlobHandler
+    {
+        // Retrieve storage account from connection string.
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+        CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+        private string imageDirecoryUrl;
+
+        /// <summary>
+        /// Receives the users Id for where the pictures are and creates 
+        /// a blob storage with that name if it does not exist.
+        /// </summary>
+        /// <param name="imageDirecoryUrl"></param>
+        public BlobHandler(string imageDirecoryUrl)
+        {
+            this.imageDirecoryUrl = imageDirecoryUrl;
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container. 
+            CloudBlobContainer container = blobClient.GetContainerReference(imageDirecoryUrl);
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            //Make available to everyone
+            container.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+        }
+
+        //public void Upload(IEnumerable<HttpPostedFileBase> file)
+        public string Upload(HttpPostedFileBase file)
+        {
+            string blobUri = null;
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container. 
+            CloudBlobContainer container = blobClient.GetContainerReference(imageDirecoryUrl);
+
+            if (file != null)
+            {
+                //foreach (var f in file)
+                //{
+                //    if (f != null)
+                //    {
+                        //CloudBlockBlob blockBlob = container.GetBlockBlobReference(f.FileName);
+                        //blockBlob.UploadFromStream(f.InputStream);
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(file.FileName);
+                blockBlob.UploadFromStream(file.InputStream);
+                blobUri = blockBlob.Uri.ToString();
+                //    }
+                //}
+            }
+            return blobUri;
+        }
+
+        public List<string> GetBlobs()
+        {
+            // Create the blob client. 
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference(imageDirecoryUrl);
+
+            List<string> blobs = new List<string>();
+
+            // Loop over blobs within the container and output the URI to each of them
+            foreach (var blobItem in container.ListBlobs())
+                blobs.Add(blobItem.Uri.ToString());
+
+            return blobs;
         }
     }
 }
---
+++ AIMS/Models/BlobHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AIMS.Models
+{
+    
+}
